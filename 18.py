00aaf7c29795e5dcb41cf3ee581cb5b2012a5760
radicalcakes <tbhuynh10@gmail.com>
2013-06-06 20:07:20 -0400

    solved 18

---
+++ 18.py
@@ -0,0 +1,17 @@
+# use dynamic programming to find shortest path
+def find_sum(triangle):
+    index = len(triangle) - 2
+    for x in xrange(index, -1, -1):
+        for y in xrange(0, x+1):
+            triangle[x][y] += max(triangle[x + 1][y], triangle[x + 1][y + 1])
+            # a + max(b,c)
+    return triangle[x][y]
+
+
+
+if __name__ == "__main__":
+    text = open("triangle_18.txt", "r")
+    triangle = []
+    for line in text:
+        triangle.append([int(num) for num in line.split()])
+    print find_sum(triangle)
